2288e7cea59e40f6a34677809dc2aae4f7fadf28
StrongAle <alessio.falchi90@gmail.com>
2024-02-09 17:45:16 +0000

    port change

--- src/server/main.js
+++ src/server/main.js
@@ -1,17 +1,19 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import 'dotenv/config'
+
 
 const app = express();
+ViteExpress.config({ mode: "production" })
 
 import { data } from "./data.js";
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + Vue!");
-});
 app.get("/api/page", (req, res) => {
   res.send(data);
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+const port = process.env.PORT || 3000;
+
+ViteExpress.listen(app, port, () =>
+  console.log("Server is listening on port " + port + "..."),
 );
